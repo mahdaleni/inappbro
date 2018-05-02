9790ccbd52a882c6fca9fc05da35045bb91c906b
Arif <dewisandrax@gmail.com>
2018-05-02 15:16:11 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -37,10 +37,7 @@ var app = {
 
         var ref = window.open('https://infopemilu.kpu.go.id/pilkada2018/pemilih/dpt/1/RIAU', '_blank', 'location=yes');
 
-        ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
-        ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-        ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-        ref.addEventListener('exit', function(event) { alert(event.type); });
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
