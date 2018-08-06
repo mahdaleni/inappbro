ec5ad3ac692810b108aff5a2d50026f3b9333a2e
Arif <dewisandrax@gmail.com>
2018-08-06 10:15:29 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        var ref = window.open('https://jastek.privatecode.ml', '_blank', 'location=no');
+        var ref = window.open('http://jastek.privatecode.ml', '_blank', 'location=no');
 
         
     },
